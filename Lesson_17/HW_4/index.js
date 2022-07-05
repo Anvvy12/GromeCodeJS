c98f46cb44242f276bee5f91a54383bd7de87a0b
Bogdan <youbody7@gmail.com>
2022-07-05 19:02:56 +0300

    s

---
+++ Lesson_17/HW_4/index.js
@@ -0,0 +1,23 @@
+// put your code here
+
+const user = {
+  getFullName() {
+    console.log(`${this.firstName} ${this.lastName}`);
+    return `${this.firstName} ${this.lastName}`;
+  },
+  setFullName(name) {
+    const nameArray = name.split(" ");
+    [user.firstName, user.lastName] = nameArray;
+  },
+};
+
+// test Data
+
+user.setFullName("John Doe");
+user.getFullName();
+//----------------
+user.setFullName("Eminem Bereminem");
+user.getFullName();
+//----------------
+user.setFullName("Mark Klark");
+user.getFullName();
