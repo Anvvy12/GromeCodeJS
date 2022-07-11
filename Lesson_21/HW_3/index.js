12c0dc88399ffee4de8e52eb6d6d71dc37daa6cf
Bogdan <youbody7@gmail.com>
2022-07-11 16:18:06 +0300

    s

---
+++ Lesson_21/HW_3/index.js
@@ -0,0 +1,26 @@
+export function getTitle() {
+  const title = document.querySelector(".title");
+  const returnedTitleText = title.textContent;
+  return returnedTitleText;
+}
+
+export function getDescription() {
+  const description = document.querySelector(".about");
+  const returnedDescriptionText = description.innerText;
+  return returnedDescriptionText;
+}
+
+export function getPlans() {
+  const plans = document.querySelector(".plans");
+  const returnedPlansText = plans.innerHTML;
+  return returnedPlansText;
+}
+export function getGoal() {
+  const goal = document.querySelector(".goal");
+  const returnedGoalText = goal.innerHTML;
+  return returnedGoalText;
+}
+console.log(getTitle());
+console.log(getDescription());
+console.log(getPlans());
+console.log(getGoal());
