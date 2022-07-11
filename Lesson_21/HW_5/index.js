532f740b0db19364016b89ef00e6bb844b8a1a91
Bogdan <youbody7@gmail.com>
2022-07-11 17:11:38 +0300

    s

--- Lesson_21/HW_5/index.js
+++ Lesson_21/HW_5/index.js
@@ -1,9 +1,11 @@
-function setButton(buttonText) {
+export function setButton(buttonText) {
   const dockBody = document.querySelector("body");
+
   dockBody.innerHTML = "<button>" + buttonText + " </button>";
 
   //   const dockBodyV2 = document.querySelector("body");
   //   dockBody.textContent = `<button> ${buttonText} </button>`;
 }
 // Test data
+
 // setButton("button text");
