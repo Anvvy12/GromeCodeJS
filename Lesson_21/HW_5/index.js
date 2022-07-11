0e13e09dc61dc51ad45091eb206b916d160afdb3
Bogdan <youbody7@gmail.com>
2022-07-11 16:59:05 +0300

    s

---
+++ Lesson_21/HW_5/index.js
@@ -0,0 +1,9 @@
+export function setButton(buttonText) {
+  const dockBody = document.querySelector("body");
+  dockBody.innerHTML = `<button> ${buttonText} </button>`;
+
+  //   const dockBodyV2 = document.querySelector("body");
+  //   dockBodyV2.textContent = `<button> ${buttonText} </button>`;
+}
+
+setButton("buttonText");
