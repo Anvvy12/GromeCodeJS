c069e4eb081fb2651e73b40a831ce43395c448ab
Bogdan <youbody7@gmail.com>
2022-07-12 19:03:01 +0300

    s

--- Lesson_21/HW_12/index.js
+++ Lesson_21/HW_12/index.js
@@ -1,1 +1,18 @@
-function getSection(num) {}
+export function getSection(num) {
+  Number(num);
+  if (num >= 7 || num <= 0) {
+    return "Wrong input!";
+  }
+  const fouderSpan = document.querySelector(`span[data-number="${num}"]`);
+  return fouderSpan.closest(".box");
+}
+// test data
+// console.log(getSection("1"));
+// console.log(getSection("2"));
+// console.log(getSection("3"));
+// console.log(getSection("4"));
+// console.log(getSection("5"));
+// console.log(getSection("6"));
+// console.log(getSection("-1"));
+// console.log(getSection("7"));
+// console.log(getSection("0"));
