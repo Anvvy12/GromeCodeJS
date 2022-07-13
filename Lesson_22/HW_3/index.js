42af321ca89349a884f09530e7b8c4bb389fa267
Bogdan <youbody7@gmail.com>
2022-07-13 15:23:37 +0300

    s

--- Lesson_22/HW_3/index.js
+++ Lesson_22/HW_3/index.js
@@ -1,4 +1,2 @@
 const checkBox = document.querySelector(".task-status");
-checkBox.addEventListener("change", () =>
-  console.log(checkBox.checked, checkBox.value)
-);
+checkBox.addEventListener("change", () => console.log(checkBox.checked));
