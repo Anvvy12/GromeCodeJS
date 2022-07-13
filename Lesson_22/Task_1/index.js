5ab1135ea2882d4e2a58a85a74090b345ced145e
Bogdan <youbody7@gmail.com>
2022-07-13 20:04:06 +0300

    s

--- Lesson_22/Task_1/index.js
+++ Lesson_22/Task_1/index.js
@@ -22,12 +22,12 @@ const logTarget = (text, color) => {
 
 // uotPut ----------------
 
-const logGreyDiv = logTarget.bind(null, "DIV", "grey");
-const logGreyP = logTarget.bind(null, "P", "grey");
-const logGreySpan = logTarget.bind(null, "SPAN", "grey");
-const logGreenDiv = logTarget.bind(null, "DIV", "green");
-const logGreenP = logTarget.bind(null, "P", "green");
-const logGreenSpan = logTarget.bind(null, "SPAN", "green");
+const logGreyDiv = logTarget.bind(null, "DIV", "green");
+const logGreyP = logTarget.bind(null, "P", "green");
+const logGreySpan = logTarget.bind(null, "SPAN", "green");
+const logGreenDiv = logTarget.bind(null, "DIV", "grey");
+const logGreenP = logTarget.bind(null, "P", "grey");
+const logGreenSpan = logTarget.bind(null, "SPAN", "grey");
 
 const removeBtnFunc = () => {
   elemDiv.removeEventListener("click", logGreyDiv);
