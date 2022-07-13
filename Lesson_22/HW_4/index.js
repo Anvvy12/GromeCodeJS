07765e61bcdf20aa31c6e66ea91b2725ad8a07d2
Bogdan <youbody7@gmail.com>
2022-07-13 15:29:39 +0300

    s

---
+++ Lesson_22/HW_4/index.js
@@ -0,0 +1,8 @@
+const input = document.querySelector(".search__input");
+const LogBtn = document.querySelector(".search__btn");
+
+const inputText = input.addEventListener("change", () =>
+  console.log(input.value)
+);
+
+LogBtn.addEventListener("click", inputText());
