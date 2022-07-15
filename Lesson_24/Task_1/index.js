b7d2e813535f0320b69860904b85b97ea00d7d8d
Bogdan <youbody7@gmail.com>
2022-07-15 21:16:30 +0300

    a

--- Lesson_24/Task_1/index.js
+++ Lesson_24/Task_1/index.js
@@ -3,7 +3,8 @@ const dayOfWeek = ["Su", "Mo", "Tu", "We", "Th", "Fr", "St"];
 function getDayOfWeek(date, days) {
   const day = new Date(date).getDate();
   const dayInFuture = new Date(date).setDate(day + days);
-  return new Date(dayInFuture).getDay();
+  const result = new Date(dayInFuture).getDay();
+  return dayOfWeek[result];
 }
 const result = getDayOfWeek(new Date(2019, 0, 1), 14);
-console.log(dayOfWeek[result]);
+console.log(result);
