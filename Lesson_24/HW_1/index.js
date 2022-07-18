a6893e1782d93b1ac89bda4d83b33154698bfea5
Bogdan <youbody7@gmail.com>
2022-07-18 20:23:14 +0300

    z

--- Lesson_24/HW_1/index.js
+++ Lesson_24/HW_1/index.js
@@ -1,7 +1,8 @@
 export function getDiff(start, end) {
-  const day = Math.round((end - start) / 1000 / 60 / 60 / 24);
-  const hour = Math.round(((end - start) / 1000 / 60 / 60) % 24);
-  const minute = Math.round(((end - start) / 1000 / 60) % 60);
-  const second = Math.round(((end - start) / 1000) % 60);
+  const dateDif = (end - start) / 1000;
+  const day = Math.round(dateDif / 60 / 60 / 24);
+  const hour = Math.round((dateDif / 60 / 60) % 24);
+  const minute = Math.round((dateDif / 60) % 60);
+  const second = Math.round(dateDif % 60);
   return `${day}d ${hour}h ${minute}m ${second}s`;
 }
