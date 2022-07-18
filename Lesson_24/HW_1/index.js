378d98ca525ed020b19276ed5650ff1cf61ea3c7
Bogdan <youbody7@gmail.com>
2022-07-18 20:38:28 +0300

    z

--- Lesson_24/HW_1/index.js
+++ Lesson_24/HW_1/index.js
@@ -6,3 +6,9 @@ export function getDiff(start, end) {
   const second = Math.round(dateDif % 60);
   return `${day}d ${hour}h ${minute}m ${second}s`;
 }
+
+const date1 = new Date(2001, 7, 17, 12, 0, 0);
+const date2 = new Date(2001, 7, 17, 12, 61, 61);
+
+const res = getDiff(date1, date2);
+console.log(res);
