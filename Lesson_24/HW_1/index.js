0846a1283c65c00282ca64fc8ac9fff8ab53e6b4
Bogdan <youbody7@gmail.com>
2022-07-18 20:54:23 +0300

    s

--- Lesson_24/HW_1/index.js
+++ Lesson_24/HW_1/index.js
@@ -1,15 +1,25 @@
 export function getDiff(startDate, endDate) {
   const dateDif = (endDate - startDate) / 1000;
-  const day = Math.floor(dateDif / 60 / 60 / 24);
-  const hour = Math.floor((dateDif / 60 / 60) % 24);
-  const minute = Math.floor((dateDif / 60) % 60);
-  const second = Math.floor(dateDif % 60);
-  //   return `${day}d ${hour}h ${minute}m ${second}s`;
-  return day + "d " + hour + "h " + minute + "m " + second + "s";
+  let res;
+  if (dateDif > 0) {
+    const day = Math.floor(dateDif / 60 / 60 / 24);
+    const hour = Math.floor((dateDif / 60 / 60) % 24);
+    const minute = Math.floor((dateDif / 60) % 60);
+    const second = Math.floor(dateDif % 60);
+    res = day + "d " + hour + "h " + minute + "m " + second + "s";
+  } else {
+    const day = Math.ceil(dateDif / 60 / 60 / 24);
+    const hour = Math.ceil((dateDif / 60 / 60) % 24);
+    const minute = Math.ceil((dateDif / 60) % 60);
+    const second = Math.ceil(dateDif % 60);
+    res = day + "d " + hour + "h " + minute + "m " + second + "s";
+  }
+
+  return res;
 }
 
-const date1 = new Date("September 12 2011 13:13");
-const date2 = new Date("September 14 2011 13:13");
+const date1 = new Date("September 1 2011 13:13");
+const date2 = new Date("September 14 2011 13:12");
 
 const res = getDiff(date1, date2);
 console.log(res);
