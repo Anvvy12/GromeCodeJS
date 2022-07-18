a1cf964e961b62531a2b90861628665360517c05
Bogdan <youbody7@gmail.com>
2022-07-18 22:03:19 +0300

    z

--- Lesson_24/HW_1/index.js
+++ Lesson_24/HW_1/index.js
@@ -1,17 +1,17 @@
 export const getDiff = (startDate, endDate) => {
   console.log(endDate - startDate);
-  const dateDif = (endDate - startDate) / 10000;
+  const dateDif = (endDate - startDate) / 1000;
   console.log(dateDif);
 
-  const day = Math.trunc(dateDif / 60 / 60 / 24);
-  const hour = Math.trunc((dateDif / 60 / 60) % 24);
-  const minute = Math.trunc((dateDif / 60) % 60);
-  const second = Math.trunc(dateDif % 60);
+  const day = Math.floor(dateDif / 60 / 60 / 24);
+  const hour = Math.floor((dateDif / 60 / 60) % 24);
+  const minute = Math.floor((dateDif / 60) % 60);
+  const second = Math.floor(dateDif % 60);
   return `${day}d ${hour}h ${minute}m ${second}s`;
 };
 
 const date1 = new Date("September 1 2011 13:13");
-const date2 = new Date("September 14 2011 13:12");
+const date2 = new Date("September 14 2011 13:13:12");
 
 const res = getDiff(date1, date2);
 console.log(res);
