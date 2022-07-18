4a434841a136ae413bc86e16ee29b569c83c871e
Bogdan <youbody7@gmail.com>
2022-07-18 21:27:41 +0300

    s

--- Lesson_24/HW_1/index.js
+++ Lesson_24/HW_1/index.js
@@ -1,35 +1,36 @@
-// export const getDiff = (startDate, endDate) => {
-//   const dateDif = (endDate - startDate) / 1000;
+export const getDiff = (startDate, endDate) => {
+  const dateDif = (endDate.getTime() - startDate.getTime()) / 1000;
 
-//   const day = Math.ceil(dateDif / 60 / 60 / 24);
-//   const hour = Math.ceil((dateDif / 60 / 60) % 24);
-//   const minute = Math.ceil((dateDif / 60) % 60);
-//   const second = Math.ceil(dateDif % 60);
-//   return day + "d " + hour + "h " + minute + "m " + second + "s";
-// };
+  const day = Math.ceil(dateDif / 60 / 60 / 24);
+  const hour = Math.ceil((dateDif / 60 / 60) % 24);
+  const minute = Math.ceil((dateDif / 60) % 60);
+  const second = Math.ceil(dateDif % 60);
+  return day + "d " + hour + "h " + minute + "m " + second + "s";
+};
 
-// const date1 = new Date("September 1 2011 13:13");
-// const date2 = new Date("September 14 2011 13:12");
+const date1 = new Date("September 1 2011 13:13");
+const date2 = new Date("September 14 2011 13:12");
 
-// const res = getDiff(date1, date2);
-// console.log(res);
-//=========================
+const res = getDiff(date1, date2);
+console.log(res);
 
-export function getDiff(date1, date2) {
-  let diff = (date2 - date1) / 1000;
-  diff = Math.abs(Math.floor(diff));
+// =========================
 
-  const days = Math.floor(diff / (24 * 60 * 60));
-  let leftSec = diff - days * 24 * 60 * 60;
+// export function getDiff(date1, date2) {
+//   let diff = (date2 - date1) / 1000;
+//   diff = Math.abs(Math.floor(diff));
 
-  const hrs = Math.floor(leftSec / (60 * 60));
-  leftSec -= hrs * 60 * 60;
+//   const days = Math.floor(diff / (24 * 60 * 60));
+//   let leftSec = diff - days * 24 * 60 * 60;
 
-  const min = Math.floor(leftSec / 60);
-  leftSec -= min * 60;
+//   const hrs = Math.floor(leftSec / (60 * 60));
+//   leftSec -= hrs * 60 * 60;
 
-  return days + "d " + hrs + "h " + min + "m " + leftSec + "s";
-}
+//   const min = Math.floor(leftSec / 60);
+//   leftSec -= min * 60;
+
+//   return days + "d " + hrs + "h " + min + "m " + leftSec + "s";
+// }
 
 // const date1 = new Date();
 // const date2 = new Date("2015/07/30 21:59:00");
