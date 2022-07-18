f0dc0a005fcb15889f9da7bfdbcf1f38577cdbb8
Bogdan <youbody7@gmail.com>
2022-07-18 20:21:27 +0300

    s

--- Lesson_24/HW_1/index.js
+++ Lesson_24/HW_1/index.js
@@ -5,7 +5,3 @@ export function getDiff(start, end) {
   const second = Math.round(((end - start) / 1000) % 60);
   return `${day}d ${hour}h ${minute}m ${second}s`;
 }
-const startDate = new Date(1994, 2, 12, 4, 0, 0);
-const endDate = new Date(1995, 12, 10, 2, 1, 1);
-
-console.log(getDiff(startDate, endDate));
