baebf847230992f23284a464881933a68731337f
Bogdan <youbody7@gmail.com>
2022-07-20 15:18:28 +0300

    s

---
+++ Lesson_28/Task_2/index.js
@@ -0,0 +1,36 @@
+const favorites = ["id-2"];
+const tree = {
+  id: "id-1",
+  name: "Products",
+  nodes: [
+    {
+      id: "id-2",
+      name: "Food",
+      nodes: [],
+    },
+  ],
+};
+
+const markFavorites = (tree, favorites) => {
+  const isFavorite = favorites.includes(tree.id);
+  return {
+    ...tree,
+    favorites: isFavorite,
+    nodes: tree.nodes.map((childNodex) => markFavorites(childNodex, favorites)),
+  };
+};
+const result = markFavorites(tree, favorites);
+console.log(result);
+// result;  {
+//     id: 'id-1',
+//     name: 'Products',
+//     isFavorite: false,
+//     nodes: [
+//         {
+//             id: 'id-2',
+//             isFavorite: true,
+//             name: 'Food',
+//             nodes: []
+//         },
+//     ],
+// };
