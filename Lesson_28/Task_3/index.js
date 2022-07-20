9920785163f5075178477ec9e85923fd72acc0fc
Bogdan <youbody7@gmail.com>
2022-07-20 15:42:24 +0300

    s

---
+++ Lesson_28/Task_3/index.js
@@ -0,0 +1,10 @@
+export const mult = (a) => (b) => a * b;
+
+export const twice = mult(2);
+
+export const triple = mult(3);
+
+// test data
+// console.log(mult(2)(4));
+// console.log(twice(7));
+// console.log(triple(4));
