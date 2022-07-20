e9c8087ea21c8e423007fb71f3d22d232aab0ac7
Bogdan <youbody7@gmail.com>
2022-07-20 18:00:12 +0300

    s

---
+++ Lesson_29/Task_1/index.js
@@ -0,0 +1,35 @@
+const addImage = (imgSrc, callack) => {
+  // put your code here
+  const imgElem = document.createElement("img");
+  imgElem.setAttribute("alt", "somePhoto");
+
+  imgElem.src = imgSrc;
+
+  const containerElem = document.querySelector(".page");
+  containerElem.append(imgElem);
+
+  const onImageLoaded = () => {
+    const { width, height } = imgElem;
+    callack(null, { width, height });
+  };
+
+  imgElem.addEventListener("load", onImageLoaded);
+};
+
+// callack function
+const onImageLoaded = (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const { width, height } = data;
+
+  const sizeElem = document.querySelector(".image-size");
+
+  sizeElem.textContent = `${width} x ${height}`;
+};
+// examples
+addImage(
+  "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg",
+  onImageLoaded
+);
