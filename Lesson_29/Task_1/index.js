8faa58f0283ce26a489f89df84dd8f53b291916c
Bogdan <youbody7@gmail.com>
2022-07-21 15:43:39 +0300

    s

--- Lesson_29/Task_1/index.js
+++ Lesson_29/Task_1/index.js
@@ -26,7 +26,6 @@ function onImageLoaded(error, data) {
   sizeElem.textContent = `${width} x ${height}`;
 }
 
-addImage(
-  "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg",
-  onImageLoaded
-);
+const imgSrc =
+  "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg";
+addImage(imgSrc, onImageLoaded);
