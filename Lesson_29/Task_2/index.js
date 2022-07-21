ad1368bc8473f2f1684d9a6fe07b3b8a61b7ab77
Bogdan <youbody7@gmail.com>
2022-07-21 17:09:29 +0300

    s

--- Lesson_29/Task_2/index.js
+++ Lesson_29/Task_2/index.js
@@ -1,32 +1,16 @@
-function addImage(imgSrc, callback) {
-  const imgElem = document.createElement("img");
-  imgElem.setAttribute("alt", "My photo");
+const pinger = (count, period) => {
+  // put your code here
+  let i = count;
+  console.log("Ping");
+  const interval = setInterval(() => {
+    if (--i > 0) {
+      console.log("Ping");
+    } else {
+      clearInterval(interval);
+    }
+  }, period);
+};
 
-  imgElem.src = imgSrc;
-  const containerElem = document.querySelector(".page");
-  console.log(imgElem);
-  containerElem.append(imgElem);
-
-  function onImageLoaded() {
-    const { width, height } = imgElem;
-    callback(null, { width, height });
-  }
-
-  imgElem.addEventListener("load", onImageLoaded);
-  imgElem.addEventListener("error", () => callback("Image load failed"));
-}
-
-function onImageLoaded(error, data) {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  const { width, height } = data;
-  const sizeElem = document.querySelector(".image-size");
-  sizeElem.textContent = `${width} x ${height}`;
-}
-
-const imgLink =
-  "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg";
-addImage(imgLink, onImageLoaded);
+// examples
+pinger(5, 100); // makes 5 writes with 100 ms interval
+// pinger(7, 150); // makes 7 writes with 1500 ms interval
