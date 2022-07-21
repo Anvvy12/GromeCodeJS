5b1cf05b59325c1750ed6df2f2d87feafeddb2a0
Bogdan <youbody7@gmail.com>
2022-07-21 15:42:49 +0300

    s

--- Lesson_29/Task_1/index.js
+++ Lesson_29/Task_1/index.js
@@ -25,3 +25,8 @@ function onImageLoaded(error, data) {
   const sizeElem = document.querySelector(".image-size");
   sizeElem.textContent = `${width} x ${height}`;
 }
+
+addImage(
+  "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg",
+  onImageLoaded
+);
