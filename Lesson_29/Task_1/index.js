09e7660dd2b39ba9611053d3d64e4af3d6e4ce3e
Bogdan <youbody7@gmail.com>
2022-07-21 15:38:53 +0300

    s

--- Lesson_29/Task_1/index.js
+++ Lesson_29/Task_1/index.js
@@ -1,24 +1,22 @@
-export const addImage = (imgSrc, callback) => {
-  // put your code here
+export function addImage(imgSrc, callback) {
   const imgElem = document.createElement("img");
   imgElem.setAttribute("alt", "My photo");
 
   imgElem.src = imgSrc;
-
   const containerElem = document.querySelector(".page");
   containerElem.append(imgElem);
 
-  const onImageLoaded = () => {
+  function onImageLoaded() {
     const { width, height } = imgElem;
     callback(null, { width, height });
-  };
+  }
 
   imgElem.addEventListener("load", onImageLoaded);
   imgElem.addEventListener("error", callback("Image load failed"));
-};
+}
 
 // callack function
-const onImageLoaded = (error, data) => {
+function onImageLoaded(error, data) {
   if (error) {
     console.log(error);
     return;
@@ -26,9 +24,4 @@ const onImageLoaded = (error, data) => {
   const { width, height } = data;
   const sizeElem = document.querySelector(".image-size");
   sizeElem.textContent = `${width} x ${height}`;
-};
-// examples;
-
-const imgSrc =
-  "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg";
-addImage(imgSrc, onImageLoaded);
+}
