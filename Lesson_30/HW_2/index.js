db2b8ef8850af4cc45e99689e8cdf40970fb7bbb
Bogdan <youbody7@gmail.com>
2022-07-21 21:22:59 +0300

    s

--- Lesson_30/HW_2/index.js
+++ Lesson_30/HW_2/index.js
@@ -1,34 +1,37 @@
-/* здесь мы создаем промис, который резолвит объект { name: 'Tom', age: 17 } через 1 секунду */
-const userDataPromise = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve({ name: "Tom", age: 17 });
-  }, 1000);
+/* ===> 1 <=== */
+
+/*
+ * successPromise должен зарезолвить число 67
+ * Ответьте себе на вопрос, какой тип данных имеет переменная successPromise
+ */
+
+const successPromise = new Promise((resolve) => {
+  /* ...code here */
+  resolve(67);
 });
 
-console.log(userDataPromise.then());
+console.log(successPromise.then());
+console.log(typeof successPromise.then());
+console.log(typeof successPromise);
+
+// Object 0_0 ??? Okey
 
 /*
- * Выведите в консоль переменную userDataPromise
- * Ответьте себе на вопрос, какой тип данных имеет переменная userDataPromise?
+ * допишите обработчик успешного промиса (аргументы и тело ф-ции onSuccess)
+ * чтобы в консоль вывелся квадрат числа, которое резолвиться из successPromise
  */
 
-/* ...code here */
-
-/*
- * Выведите в консоль переменную userData в обработчике промиса
- * Ответьте себе на вопрос, что находится в переменной userData?
- */
-userDataPromise.then(function onSuccess(userData) {
+successPromise.then(function onSuccess(number) {
   /* ...code here */
+  console.log(number ** 2);
 });
 
 /*
- * подпишитесь на успешное выполнение промиса userDataPromise
- * используйте метод .then
- * в обработчике промиса (ф-ция внутри .then() ) выведите в консоль строку 'My name is Tom. I am 17 years old'
- * Tom и 17 достаньте с данных, которые приходят в ф-цию onSuccess
+ * в обработчике ошибок промиса (ф-ция onError внутри .catch()) выведите в консоль текст 'I am an error'
+ * текст вывелся в консоль?
+ * подумайте почему
  */
-
-userDataPromise.then(function onSuccess(/* ...code here */) {
+successPromise.catch(function onError() {
   /* ...code here */
+  console.log("I am an error");
 });
