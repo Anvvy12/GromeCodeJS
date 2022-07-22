7be980ad561c9e952ceb4a488e5504e219e044bd
Bogdan <youbody7@gmail.com>
2022-07-22 17:07:13 +0300

    s

--- Lesson_32/Task_1/index.js
+++ Lesson_32/Task_1/index.js
@@ -2,6 +2,7 @@ const getValueWithDelay = (number, time) => {
   const sumPromise = new Promise((resolve, reject) => {
     if (isNaN(number)) {
       reject(new Error("Can't calculate"));
+      return;
     }
     setTimeout(() => {
       console.log(number);
@@ -19,9 +20,11 @@ export const asyncSum = (...numbers) => {
   return Promise.all(numbers).then((value) => getSum(value));
 };
 
-const number1 = getValueWithDelay(12, 1000);
+const number1 = getValueWithDelay("a", 1000);
 const number2 = getValueWithDelay(15, 2000);
 const number3 = getValueWithDelay(5, 3000);
 const number4 = getValueWithDelay(3, 2000);
 
-asyncSum(number1, number2, number3, number4).then((res) => console.log(res));
+asyncSum(number1, number2, number3, number4)
+  .then((res) => console.log(res))
+  .catch((error) => console.log(error));
