93c0011aa3dca56af17e1d25863cd88f00d3ef5c
Bogdan <youbody7@gmail.com>
2022-07-22 15:12:51 +0300

    s

---
+++ Lesson_31/Task_2/index.js
@@ -0,0 +1,26 @@
+export const asyncCalculator = (number) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`Initial value: ${number}`);
+      resolve(number);
+    }, 500);
+  })
+    .then(
+      (data) =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            const res = data ** 2;
+            console.log(`Squared value: ${res}`);
+            resolve(res);
+          }, 500);
+        })
+    )
+    .then(
+      (data) =>
+        new Promise((resolve) => {
+          const res = data * 2;
+          console.log(`Doubled value: ${res}`);
+          resolve(res);
+        })
+    );
+const resCalc = asyncCalculator(5);
