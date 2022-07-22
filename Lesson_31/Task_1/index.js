2596c0d4c527660ffd987d081f73e1b951fa3c38
Bogdan <youbody7@gmail.com>
2022-07-22 14:26:50 +0300

    s

---
+++ Lesson_31/Task_1/index.js
@@ -0,0 +1,28 @@
+/*
+ * @param {string} userId
+ * @return {promise}
+ */
+export const requestUserData = (userId) => {
+  // put your code here
+  const promise = new Promise((resolve, reject) => {
+    if (userId === "broken") {
+      setTimeout(() => {
+        reject(new Error("User not found"));
+      }, 500);
+    } else {
+      setTimeout(() => {
+        resolve({
+          name: "John",
+          age: 17,
+          userId,
+          email: "userid777@example.com",
+        });
+      }, 1000);
+    }
+  });
+
+  return promise;
+};
+
+const res = requestUserData("14");
+res.then((date) => console.log(date));
