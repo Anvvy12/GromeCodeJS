0b6223624644e1ca6ea4cab4671f4ec26495d33a
Bogdan <youbody7@gmail.com>
2022-07-25 18:14:56 +0300

    s

---
+++ Lesson_33/HW_1/index.js
@@ -0,0 +1,28 @@
+const baseUrl = "https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks";
+
+export function getTasksList() {
+  // put your code here
+  return fetch(baseUrl).then((data) => data.json());
+}
+
+export function getTaskById(taskId) {
+  return fetch(baseUrl).then((date) => {
+    const dateJ = date.json();
+    console.log(dateJ);
+    dateJ.forEach((obj) => {
+      if (obj.id === taskId) {
+        return obj;
+      }
+      return;
+    });
+  });
+}
+
+// examples
+// getTasksList().then((tasksList) => {
+//   console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
+// });
+
+// getTaskById("2").then((taskData) => {
+//   console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
+// });
