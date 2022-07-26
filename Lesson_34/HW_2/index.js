f153dd571a5533bc01ff3439060f7199c5802bb1
Bogdan <youbody7@gmail.com>
2022-07-26 21:43:10 +0300

    s

--- Lesson_34/HW_2/index.js
+++ Lesson_34/HW_2/index.js
@@ -1,4 +1,4 @@
-const URL = "https://62e0189198dd9c9df60da24a.mockapi.io";
+const url = "https://62e0189198dd9c9df60da24a.mockapi.io";
 
 const loginFormElem = document.querySelector(".login-form");
 const submitBtn = document.querySelector(".submit-button");
@@ -13,23 +13,27 @@ const validate = () => {
 };
 
 loginFormElem.addEventListener("input", validate);
-const getUsersDate = () => {
-  fetch(URL).then((elem) => alert(elem));
-};
+
 const postUserDate = () => {
   const formData = [Object.fromEntries(new FormData(loginFormElem))].reduce(
-    (acc, [input, value]) => ({ ...acc, [input]: value }),
+    (acc, [input, value]) => Object.assign(acc, { [input]: value }),
     {}
   );
   console.log(formData);
 
-  fetch(URL, {
+  fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
     body: JSON.stringify(formData),
-  }).then((elem) => alert(elem));
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      inputsArr.map((ele) => (ele.value = ""));
+      submitBtn.disabled = true;
+      alert(JSON.stringify(response));
+    });
 };
 
 submitBtn.addEventListener("submit", postUserDate);
