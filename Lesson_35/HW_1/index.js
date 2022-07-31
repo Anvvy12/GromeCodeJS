edeeba23d2d3bf4ba50f255872a669a9ab4bb145
Bogdan <youbody7@gmail.com>
2022-07-31 17:36:26 +0300

    s

--- Lesson_35/HW_1/index.js
+++ Lesson_35/HW_1/index.js
@@ -1,11 +1,25 @@
 const jsonString = '{"name": "Ben", "age": 24}';
 
-export const parseUser = (string) => {
-  try {
-    return JSON.parse(string);
-  } catch (err) {
-    return null;
-  }
-};
+// export const parseUser = (string) => {
+//   try {
+//     return JSON.parse(string);
+//   } catch (err) {
+//     return null;
+//   }
+// };
 
 console.log(parseUser(jsonString));
+
+const incaps = (a) => {
+  return a;
+};
+const incaps = (a, b) => {
+  return a + b;
+};
+const incaps = (a, b, c) => {
+  return a + b + c;
+};
+
+incaps(2);
+incaps(2, 5);
+incaps(2, 5, 10);
---
+++ Lesson_36/Task_1/index.js
@@ -0,0 +1,19 @@
+const url = "https://api.github.com/users";
+
+const fetchUser = async (userId) => {
+  // put your code here
+  try {
+    const response = await fetch(`${url}/${userId}`);
+    if (!response.ok) {
+      return null;
+    }
+
+    const userdata = await response.json();
+    console.log(userdata);
+    return userdata;
+  } catch (e) {
+    throw new Error("Failed to get user data");
+    console.log("Failed to get user data");
+  }
+};
+fetchUser("Anvvy12");
